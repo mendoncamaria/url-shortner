fe7588601bc0547250ff12b244012e0977d574a9
Maria Mendonca <maria.mendonca156@gmail.com>
2024-09-11 12:14:34 +0530

    updated code to fetch and short url

--- api/models/url.go
+++ api/models/url.go
@@ -1,6 +1,13 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+	"math/rand"
+	"time"
+)
+
+const shortCodeLength = 6
 
 type URL struct {
 	ID          int
@@ -9,8 +16,37 @@ type URL struct {
 }
 
 func (u *URL) Save(db *sql.DB) error {
-	// Implement the logic to save the URL to the database
+	stmt, err := db.Prepare("INSERT INTO urls (original_url, short_code) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(u.OriginalURL, u.ShortCode)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-// ... other methods for retrieving URLs, generating short codes, etc.
+func GetOriginalURLByShortCode(db *sql.DB, shortCode string) (string, error) {
+	var originalURL string
+	err := db.QueryRow("SELECT original_url FROM urls WHERE short_code = ?", shortCode).Scan(&originalURL)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", errors.New("URL not found")
+		}
+		return "", err
+	}
+	return originalURL, nil
+}
+
+func generateShortCode() string {
+	rand.Seed(time.Now().UnixNano())
+	letters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, shortCodeLength)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
--- api/routes/url.go
+++ api/routes/url.go
@@ -2,15 +2,63 @@ package routes
 
 import (
 	"database/sql"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"url-shortner/api/api/models" //right import, don't change this
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 )
 
 func URLRoutes(r *chi.Router, db *sql.DB) {
-	r.Route("/urls", func(r *chi.Router) {
-		r.Post("/", createURL(db))
-		r.Get("/{shortCode}", getURL(db))
-	})
+	r.Post("/", createURL(db))
+	r.Get("/{shortCode}", getURL(db))
 }
 
-// ... implementation of createURL and getURL functions
+func createURL(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var data struct {
+			OriginalURL string `json:"original_url"`
+		}
+
+		// Decode the request body using encoding/json
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			render.Render(w, r, render.BadRequest(err))
+			return
+		}
+
+		shortCode := models.generateShortCode()
+		url := models.URL{
+			OriginalURL: data.OriginalURL,
+			ShortCode:   shortCode,
+		}
+
+		err := url.Save(db)
+		if err != nil {
+			render.Render(w, r, render.ServerError(err))
+			return
+		}
+
+		render.JSON(w, r, url)
+	}
+}
+
+func getURL(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		shortCode := chi.URLParam(r, "shortCode")
+
+		originalURL, err := models.GetOriginalURLByShortCode(db, shortCode)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) { // Handle case of non-existent URL
+				render.Render(w, r, render.NotFound(err))
+			} else {
+				render.Render(w, r, render.ServerError(err))
+			}
+			return
+		}
+
+		http.Redirect(w, r, originalURL, http.StatusFound)
+	}
+}
